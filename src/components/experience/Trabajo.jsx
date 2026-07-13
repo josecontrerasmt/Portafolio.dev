729bf7e0e599d032dae736aad5f4852cc0a6e699
Jose Contreras Martinez <josecontrerasmt@gmail.com>
2026-07-13 13:05:20 -0600

    updating tools design

--- src/components/experience/Trabajo.jsx
+++ src/components/experience/Trabajo.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import Tools from "../tools/Tools";
 
 export default function Trabajo({ data }) {
-  const [dropdown, setDropdown] = useState(true);
+  const [dropdown, setDropdown] = useState(false);
 
   return (
-    <li className="ml-4 bg-[#121313] rounded-md p-5 shadow-sm shadow-black">
+    <li className="ml-4 bg-[#121313] border border-black rounded-md p-5 shadow-sm shadow-black">
       <div className="absolute w-3 h-3 mt-2 bg-green-500 rounded-full -start-1.5"></div>
       <div className="flex justify-between gap-2 items-center mb-2.5">
         <h3 className="text-[21px] font-semibold text-gray-900 dark:text-green-400">
@@ -24,7 +24,7 @@ export default function Trabajo({ data }) {
             </>
           )}
         </h3>
-        <time className="block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+        <time className="block text-sm font-normal leading-none text-gray-400">
           {data.fechaInicio}
           <span> - </span>
           {data.fechaFin}
@@ -87,7 +87,7 @@ export default function Trabajo({ data }) {
       <div className={`w-full flex ${dropdown ? "mt-5" : "mt-2"}`}>
         <button
           onClick={() => setDropdown((prev) => !prev)}
-          className={`ml-auto opacity-70 p-1 rounded-sm flex gap-0.5 items-center hover:bg-[#212121] hover:opacity-100 transition-all text-white`}
+          className={`ml-auto text-gray-400 p-1 rounded-sm flex gap-0.5 items-center hover:bg-[#212121] hover:text-gray-100 transition-all`}
         >
           {dropdown ? (
             <>
