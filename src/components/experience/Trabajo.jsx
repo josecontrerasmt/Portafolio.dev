e1d92f9ca4b928cffcce54da6773421c8604f001
Jose Contreras Martinez <josecontrerasmt@gmail.com>
2026-07-20 13:48:11 -0600

    updating resposive design

--- src/components/experience/Trabajo.jsx
+++ src/components/experience/Trabajo.jsx
@@ -7,8 +7,8 @@ export default function Trabajo({ data }) {
   return (
     <li className="ml-4 bg-white dark:bg-[#121313] dark:border dark:border-black rounded-md p-5 shadow-sm shadow-black">
       <div className="absolute w-3 h-3 mt-2 bg-black dark:bg-green-500 rounded-full -inset-s-1.5"></div>
-      <div className="flex justify-between gap-2 items-center mb-2.5">
-        <h3 className="text-[21px] font-bold text-gray-900 dark:text-green-400">
+      <div className="flex justify-between gap-2 items-center mb-4 md:mb-2.5">
+        <h3 className="text-[18px] md:text-[21px] font-bold text-gray-900 dark:text-green-400">
           {data.titulo}
           {data.empresaLink && (
             <>
@@ -24,34 +24,34 @@ export default function Trabajo({ data }) {
             </>
           )}
         </h3>
-        <time className="block text-sm font-semibold leading-none text-gray-500">
+        <time className="block text-end text-[13px] md:text-sm font-semibold leading-none text-gray-500">
           {data.fechaInicio}
           <span> - </span>
           {data.fechaFin}
         </time>
       </div>
-      <span className="text-[16px] font-semibold block">
+      <span className="text-[14px] md:text-[16px] font-semibold block">
         {data.tituloFuncion}
       </span>
-      <ol className="flex gap-4 mt-5 w-full justify-center flex-wrap text-[13px]">
+      <ol className="flex gap-4 mt-5 w-full justify-center flex-wrap text-[13px] md:text-[14px]">
         {data?.tecnologias?.map((item) => (
           <li key={item.nombre}>
-            <Tools item={item} client:load />
+            <Tools textSize={""} item={item} client:load />
           </li>
         ))}
       </ol>
       <div
-        className={`overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] scrollbar-none grid grid-cols-2 transition-all gap-x-5 rounded-md ${dropdown ? "max-h-200 overflow-auto pointer-events-auto mt-5" : "max-h-0 overflow-hidden pointer-events-none"}`}
+        className={`md:overflow-y-scroll md:[&::-webkit-scrollbar]:hidden md:[-ms-overflow-style:none] scrollbar-none grid md:grid-cols-2 transition-all gap-x-5 gap-y-5 md:gap-y-0 rounded-md ${dropdown ? "max-h-350 overflow-auto pointer-events-auto mt-5" : "max-h-0 overflow-hidden pointer-events-none"}`}
       >
         <div>
-          <span className="block text-black dark:text-green-400 text-[18px]">
+          <span className="block text-black dark:text-green-400 text-[16px] md:text-[18px]">
             Funciones principales
           </span>
           <ol className="space-y-2 font-normal text-sm mt-3">
             {data.funciones.map((item) => (
               <li
                 key={item.text}
-                className="bg-[#ededed] dark:bg-[#1c1d1d] text-[14px] font-semibold p-2 rounded-md flex gap-2 items-center"
+                className="bg-[#ededed] dark:bg-[#1c1d1d] text-[13px] md:text-[14px] font-semibold p-2 rounded-md flex gap-2 items-center"
               >
                 <i
                   className={`${item.icon} text-black dark:text-green-500 font-bold dark:font-normal text-[16px]`}
@@ -62,14 +62,14 @@ export default function Trabajo({ data }) {
           </ol>
         </div>
         <div>
-          <span className="block text-black dark:text-green-400 text-[18px]">
+          <span className="block text-black dark:text-green-400 text-[16px] md:text-[18px]">
             Logros destacados
           </span>
           <ol className="space-y-2 font-normal text-sm mt-3">
             {data.logros.map((item) => (
               <li
                 key={item.text}
-                className="bg-[#ededed] dark:bg-[#1c1d1d] text-[14px] font-semibold p-2 rounded-md flex gap-2 items-center"
+                className="bg-[#ededed] dark:bg-[#1c1d1d] text-[13px] md:text-[14px] font-semibold p-2 rounded-md flex gap-2 items-center"
               >
                 <i
                   className={`${item.icon} text-black dark:text-green-500 font-bold dark:font-normal text-[16px]`}
@@ -92,7 +92,7 @@ export default function Trabajo({ data }) {
           />
         </div> */}
       </div>
-      <div className={`w-full flex ${dropdown ? "mt-5" : "mt-2"}`}>
+      <div className={`w-full flex ${dropdown ? "mt-5" : "mt-5 md:mt-2"}`}>
         <button
           onClick={() => setDropdown((prev) => !prev)}
           className={`ml-auto text-gray-400 p-1 rounded-sm flex gap-0.5 items-center dark:hover:bg-[#212121] dark:hover:text-gray-100 transition-all hover:text-black cursor-pointer`}
